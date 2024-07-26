040a90a6d469939e38784c43a317c5471eeb364a
Gokulegana-coder <chamoy10110@gmail.com>
2024-07-26 17:55:43 -0400

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+echo "Conectado exitosamente a la base de datos.";
+?>
